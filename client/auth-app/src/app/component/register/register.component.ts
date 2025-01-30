41af98cce08621cb2da64179eeb451667fa2e866
Nathandiv <nathan.hlungwani123@gmail.com>
2025-01-30 22:20:47 +0200

    added logic on rgister ts

--- client/auth-app/src/app/component/register/register.component.ts
+++ client/auth-app/src/app/component/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  user: User = { username: '', email: '', password: '' };
+  message: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit(): void {
+    this.authService.register(this.user).subscribe(
+      (response) => {
+        this.message = 'Registration successful!';
+      },
+      (error) => {
+        this.message = 'Registration failed.';
+      }
+    );
+  }
 
 }
--- client/auth-app/src/app/services/auth.service.ts
+++ client/auth-app/src/app/services/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  register(user: User): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, user);
+  }
+
+  login(user: User): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, user);
+  }
 }
