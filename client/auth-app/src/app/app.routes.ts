c6bb71fb26bf2bd2a9fd55a80255201bdc7ce4b7
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-31 09:40:34 +0200

    added dashbord component

--- client/auth-app/src/app/app.routes.ts
+++ client/auth-app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', component: RegisterComponent},
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', component: DashboardComponent}
     
 ];
