c3f811c8906986f142de8349b60c92ffead13d17
Nathandiv <k.n.hlungwani@gmail.com>
2025-02-13 09:59:44 +0200

    done with the multi user login and tested it

--- client/auth-app/src/app/app.routes.ts
+++ client/auth-app/src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { LoginAComponent } from './component/login-a/login-a.component';
+import { AdminDasboardComponent } from './component/admin-dasboard/admin-dasboard.component';
 
 export const routes: Routes = [
     { path: '', component: RegisterComponent},
     { path: 'login', component: LoginComponent },
     { path: 'loginA', component: LoginAComponent },
 
-    { path: 'dashboard', component: DashboardComponent}
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'admin-dashboard', component: AdminDasboardComponent}
+
+    
     
 ];
