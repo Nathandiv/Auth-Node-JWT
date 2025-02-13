f519df4a0415fd1e1f59f0a75fb775db18e6cbed
Nathandiv <k.n.hlungwani@gmail.com>
2025-02-13 09:44:18 +0200

    made changes on ath controller

--- auth-system-backend/config/config.js
+++ auth-system-backend/config/config.js
@@ -1,7 +1,6 @@
-const dotenv = require ('dotenv');
-
+const dotenv = require('dotenv');
 dotenv.config();
 
-const JWT_SECRET = proccess.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET; // ✅ Fix typo
 
 module.exports = JWT_SECRET;
